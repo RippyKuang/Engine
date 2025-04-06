ed98984ab794cca6d271f1286bd66965fe04885b
Kuang <tentpole@126.com>
2025-04-06 16:42:11 +0800

    new file:   demo/three_links.cpp

---
+++ demo/three_links.cpp
@@ -0,0 +1,54 @@
+#include <visualize.h>
+#include <iostream>
+#include <world.h>
+#include <unistd.h>
+
+//               Z    X
+//               |   /
+//               |  /
+//       Y       | /
+//       ---------/
+
+//     O------O------
+//     |
+//     |
+//     |
+//     O
+//   -----
+using namespace Engine;
+
+int main(int argc, char *argv[])
+{
+    Engine::GFrame frame(argc, argv);
+    Cube base_link(Vector3d{0.1, 0.1, 0.05});
+    Cube link0(Vector3d{0.05, 0.05, 0.2});
+    Cube link1(Vector3d{0.05, 0.05, 0.2}, Vector3d{M_PI / 2, 0, 0}, Vector3d{0, -0.1, 0});
+    Cube link2(Vector3d{0.05, 0.05, 0.2}, Vector3d{M_PI / 2, 0, 0}, Vector3d{0, -0.1, 0});
+
+    Camera camera(Vector3d{-0.5, 0, 0}, _R{300, 0, 640, 0, 300, 512, 0, 0, 1});
+    World w(camera);
+
+    Joint j0(base_link, link0, Vector3d{0, 0, 0.125}, 0, new CONTINUOUS_INFO(AXIS_Z));
+    Joint j1(link0, link1, Vector3d{0, 0, 0.125}, 1, new CONTINUOUS_INFO(AXIS_X));
+    Joint j2(link1, link2, Vector3d{0, -0.2, 0}, 2, new CONTINUOUS_INFO(AXIS_X));
+
+    w.parse_robot({j0, j1, j2});
+
+    std::vector<Engine::Point2i> projs = w.project();
+
+    frame.show();
+
+    frame.updateData(projs);
+
+    while (1)
+    {
+        usleep(50010);
+        w.drive(0, M_PI / 60);
+        w.drive(1, M_PI / 90);
+        w.drive(2, M_PI / 120);
+        std::vector<Engine::Point2i> projs = w.project();
+
+        frame.updateData(projs);
+    }
+    return 0;
+}
