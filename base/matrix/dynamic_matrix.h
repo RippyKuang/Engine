d4c9cc06b914e269f450cdfc810b0e315bb68f45
Kuang <tentpole@126.com>
2025-05-26 02:56:22 +0800

    modified:   base/matrix/dynamic_matrix.h 	modified:   main.cpp

--- base/matrix/dynamic_matrix.h
+++ base/matrix/dynamic_matrix.h
@@ -6,10 +6,14 @@ namespace Engine
 {
 
     template <typename inst, template <typename...> typename tmpl>
-    struct is_instantiation_of : std::false_type {};
-    
+    struct is_instantiation_of : std::false_type
+    {
+    };
+
     template <template <typename...> typename impl, typename... args>
-    struct is_instantiation_of<impl<args...>, impl> : std::true_type {};
+    struct is_instantiation_of<impl<args...>, impl> : std::true_type
+    {
+    };
 
     template <typename T>
     struct DynamicMatrix
@@ -20,20 +24,52 @@ namespace Engine
         DynamicMatrix(int r, int c) : rows(r), cols(c)
         {
             data = (T *)malloc(rows * cols * sizeof(T));
-        } 
+        }
+
+        Point2i get_size()
+        {
+            int r = 0, c = 0;
+            for (int i = 0; i < cols; i++)
+                c += this->data[i].get_size()[1];
+            for (int i = 0; i < rows; i++)
+                r += this->data[i].get_size()[0];
+            return Point2i(r, c);
+        }
+
+        T* at(int row, int col)
+        {
+            return &data[row * cols + col];
+        }
     };
 
-    template <typename T,typename enable = void>
-    struct find_type 
+    template <>
+    struct DynamicMatrix<double>
+    {
+        int rows;
+        int cols;
+        double *data;
+        DynamicMatrix(int r, int c) : rows(r), cols(c)
+        {
+            data = (double *)malloc(rows * cols * sizeof(double));
+        }
+
+        Point2i get_size()
+        {
+            return Point2i(rows, cols);
+        }
+
+    };
+
+    template <typename T, typename enable = void>
+    struct find_type
     {
         using type = T;
     };
-    
+
     template <typename T>
     struct find_type<T, typename std::enable_if<is_instantiation_of<T, DynamicMatrix>::value>::type>
     {
         using type = typename find_type<T>::type;
     };
 
-    
 }
--- main.cpp
+++ main.cpp
@@ -14,35 +14,12 @@ using namespace Engine;
 
 int main(int argc, char *argv[])
 {
-    const int ww = 1280;
-    const int hh = 1024;
-    Engine::GFrame frame(argc, argv, ww, hh);
-    Mesh *mesh = Mesh::load_from_file("/home/kuang/project/Engine/sphere.obj");
-    Cube base_link(Vector3d{0.05, 0.05, 0.2});
-    Link link0(std::move(*mesh));
-    base_link.set_name("base_link");
-    link0.set_name("link0");
-    Camera camera(Vector3d{-0.6, -0.3, 0.2}, _R{300, 0, 640, 0, 300, 512, 0, 0, 1});
-    World w(camera, ww, hh);
-
-    Part j0(base_link, link0, Vector3d{0, 0, 0.2 / 2 - 0.025}, new Revolute(AXIS_Z));
-
-    const Robot *robot = w.parse_robot({j0});
-    frame.show(w.get_camera_handle());
-    robot->summary();
-
-    while (1)
-    {
-        std::vector<Point2i> frame_projs;
-        std::vector<_T> t;
-        std::vector<Vector6d> v;
-        robot->FK(t, v);
-
-        w.project_frame(frame_projs, t);
-        std::vector<Engine::pixel> projs;
-        auto promise = w.project();
-        usleep(50000);
-        frame.updateFuture(std::move(promise), std::move(frame_projs));
-    }
+    DynamicMatrix<DynamicMatrix<double>> mat(2, 2);
+    mat.data[0] = DynamicMatrix<double>(2, 3);
+    mat.data[1] = DynamicMatrix<double>(2, 3);
+    mat.data[2] = DynamicMatrix<double>(1, 3);
+    mat.data[3] = DynamicMatrix<double>(1, 3);
+    std::cout<<mat.get_size()<<std::endl;
+    
     return 0;
 }
